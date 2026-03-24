8828bde579ed0b8ea3fa2479ed2db914444146c2
Lucas Orsi <lucas.orsi@contractlabs.tech>
2026-03-24 15:54:00 -0300

    feat: add main component

--- multistep_react_form/src/App.jsx
+++ multistep_react_form/src/App.jsx
@@ -1,3 +1,5 @@
+// components
+import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
 import './App.css'
 import './App.css'
 
@@ -5,7 +7,26 @@ function App() {
 
   return (
     <div className="App">
-      <h1>React Multistep</h1>
+      <div className='header'>
+        <h2>Deixe sua avaliação</h2>
+        <p>Ficamos felizes com a sua compra, utilize o formulário abaixo para avaliar o produto</p>
+
+      </div>
+      <div className="form-container">
+        <p>etapar</p>
+        <form >
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type='button'>
+              <GrFormPrevious />
+              <span>Voltar</span></button>
+            <button type='submit'>
+              <span>Avançar</span>
+              <GrFormNext />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
