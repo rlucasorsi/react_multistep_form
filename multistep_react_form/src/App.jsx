5d87bd586c8c2dd2a2119446dad984c120b13cd2
Lucas Orsi <lucas.orsi@contractlabs.tech>
2026-03-26 15:40:32 -0300

    feat: add useForm hook

--- multistep_react_form/src/App.jsx
+++ multistep_react_form/src/App.jsx
@@ -1,9 +1,24 @@
 // components
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr'
-import './App.css'
+import { FiSend } from 'react-icons/fi'
+import UserForm from "./components/UserForm";
+import ReviewForm from "./components/ReviewForm";
+import Thanks from "./components/Thanks";
+
+// hooks
+import { useForm } from "./hooks/useForm"
+
 import './App.css'
 
 function App() {
+
+  const formComponents = [
+    <UserForm />,
+    <ReviewForm />,
+    <Thanks />
+  ]
+
+  const { currentStep, currentComponent, changeStep, isLastStep, isFirstStep } = useForm(formComponents)
 
   return (
     <div className="App">
@@ -13,17 +28,27 @@ function App() {
 
       </div>
       <div className="form-container">
-        <p>etapar</p>
-        <form >
-          <div className="inputs-container"></div>
+        <p>etapas</p>
+        <form onSubmit={(e) => changeStep(currentStep + 1, e)}>
+          <div className="inputs-container">{currentComponent}</div>
           <div className="actions">
-            <button type='button'>
-              <GrFormPrevious />
-              <span>Voltar</span></button>
-            <button type='submit'>
-              <span>Avançar</span>
-              <GrFormNext />
-            </button>
+            {!isFirstStep && (
+              <button type='button' onClick={() => changeStep(currentStep - 1)}>
+                <GrFormPrevious />
+                <span>Voltar</span>
+              </button>
+            )}
+            {!isLastStep ? (
+              <button type='submit'>
+                <span>Avançar</span>
+                <GrFormNext />
+              </button>
+            ) : (
+              <button type='button'>
+                <span>Enviar</span>
+                <FiSend />
+              </button>
+            )}
           </div>
         </form>
       </div>
---
+++ multistep_react_form/src/components/UserForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserForm = () => {
+    return (
+        <div>
+            <h2>UserForm</h2>
+        </div>
+    )
+}
+
+export default UserForm
